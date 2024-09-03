7ef479705f1aa19082a4120ce85fe45208450be2
Shannon Robinson <srobin4922@gmail.com>
2024-09-03 14:39:14 -0400

    Initial app with structure and removing default app stuff

---
+++ client/weather-app/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">Fullstack Weather App</header>
+      <body className="App-body">This is where the app goes</body>
+      <footer className="App-footer">sdkjhlskdfhsf</footer>
+    </div>
+  );
+}
+
+export default App;
